e755788a9a10f4220558c9490d815c2636d94269
bri616 <bridotson@gmail.com>
2017-02-16 19:06:40 -0800

    add reset button

--- index.js
+++ index.js
@@ -6,26 +6,29 @@ import { Provider, connect } from 'react-redux'
 // React component
 class Counter extends Component {
   render() {
-    const { value, onIncreaseClick, onDecreaseClick } = this.props
+    const { value, onIncreaseClick, onDecreaseClick, onResetClick } = this.props
     return (
       <div>
         <span>{value}</span>
         <button onClick={onIncreaseClick}>Increase</button>
         <button onClick={onDecreaseClick}>Decrease</button>
+        <button onClick={onResetClick}>Reset</button>
       </div>
     )
   }
 }
 
 Counter.propTypes = {
-  value: PropTypes.number.isRequired,
-  onIncreaseClick: PropTypes.func.isRequired,
-  onDecreaseClick: PropTypes.func.isRequired
+    value: PropTypes.number.isRequired,
+    onIncreaseClick: PropTypes.func.isRequired,
+    onDecreaseClick: PropTypes.func.isRequired,
+    onResetClick: PropTypes.func.isRequired
 }
 
 // Action
 const increaseAction = { type: 'increase' }
 const decreaseAction = { type: 'decrease' }
+const resetAction = { type: 'reset' }
 
 // Reducer
 function counter(state = { count: 0 }, action) {
@@ -35,6 +38,8 @@ function counter(state = { count: 0 }, action) {
       return { count: count + 1 }
     case 'decrease':
         return { count: count - 1 }
+    case 'reset':
+        return { count: 0 }
     default:
       return state
   }
@@ -52,10 +57,11 @@ function mapStateToProps(state) {
 
 // Map Redux actions to component props
 function mapDispatchToProps(dispatch) {
-  return {
-    onIncreaseClick: () => dispatch(increaseAction),
-    onDecreaseClick: () => dispatch(decreaseAction)
-  }
+    return {
+        onIncreaseClick: () => dispatch(increaseAction),
+        onDecreaseClick: () => dispatch(decreaseAction),
+        onResetClick: () => dispatch(resetAction)
+    }
 }
 
 // Connected Component
